7250f94d552b1b455b1587664d8a3e4b93a95c5a
Juan Ignacio Cuiule <juanchicuiu@gmail.com>
2023-05-15 14:20:23 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,130 @@
-import Image from 'next/image'
+"use client";
+import { Graph } from "@/components/Graph";
+import { Interaction } from "@/components/Interaction";
+import { useInteractionStore } from "@/hooks/useInteractionStore";
+import { useToggle } from "@/hooks/useToggle";
+import { OpenAIService } from "@/utils/openai";
+import { useState } from "react";
+import { ArrowRight, Maximize, X } from "react-feather";
 
-export default function Home() {
+const InteractionView = () => {
+  const interactionStore = useInteractionStore();
+  return <Interaction interaction={interactionStore.data} />;
+};
+
+const FirstQuestionView = () => {
+  const { updateInteraction, ask } = useInteractionStore();
+
+  const [question, setQuestion] = useState("");
+  const [apiKey, setApiKey] = useState("");
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className="flex flex-col gap-10 border rounded px-4 py-4">
+      <div className="flex flex-col gap-1">
+        <label htmlFor="question-input">Tu pregunta:</label>
+        <textarea
+          id="question-input"
+          value={question}
+          placeholder="Escribí tu pregunta inicial"
+          className="border rounded text-sm px-2 inline-block w-full"
+          onChange={(e) => {
+            const value = e.target.value;
+            setQuestion(value);
+          }}
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="api-key-input">Tu api key:</label>
+        <input
+          id="api-key-input"
+          value={apiKey}
+          placeholder="Copiá acá tu api key de OpenAI. Es algo como: sk-...8BWb"
+          className="border rounded text-sm px-2 inline-block w-full"
+          onChange={(e) => {
+            const value = e.target.value;
+            setApiKey(value);
+          }}
+        />
+        <span className="text-xs mt-1 text-gray-600 ml-1">
+          Para conseguir tu api key andá a:{" "}
+          <a
+            className="text-blue-500 underline pointer hover:text-blue-800"
+            href="https://platform.openai.com/account/api-keys"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://platform.openai.com/account/api-keys
+          </a>
+        </span>
       </div>
-    </main>
-  )
+      <button
+        className="group border rounded px-3 w-fit flex items-center gap-1 hover:border-blue-500 hover:bg-blue-100"
+        style={{ transition: "width 0.2s" }}
+        onClick={() => {
+          OpenAIService.apiKey = apiKey;
+          updateInteraction("1", {
+            question,
+          });
+
+          ask({
+            id: "1",
+            question,
+            error: false,
+            loading: false,
+          });
+        }}
+      >
+        Empezar <ArrowRight className="hidden group-hover:inline w-5 h-5" />
+      </button>
+    </div>
+  );
+};
+
+export default function App() {
+  const { data } = useInteractionStore();
+
+  const [fullscreen, toggle] = useToggle(false);
+
+  return (
+    <div className="flex min-h-screen px-5 py-5 gap-4 flex-col xl:flex-row">
+      <div className="container mx-auto max-w-3xl w-full flex flex-col gap-4 align-start">
+        <details open>
+          <summary className="text-2xl">Multi ChatGPT</summary>
+
+          <p className="mt-4">
+            {">"} Descubrí y explorá temas de tu interés mediante un chat como
+            chat-gpt pero con múltiples ramificaciones.
+          </p>
+          <p className="mt-4">
+            {">"} Empezá con una pregunta y el chat te va a responder ofreciendo
+            3 o más preguntas relacionadas para seguir profundizando en el tema.
+            Podés hacer click en cualquiera de las preguntas sugeridas para
+            abrir nuevas ramas de conversación.
+          </p>
+          <p className="mt-4">
+            {">"} Siempre vas a poder ver las ramas anteriores y todo el hilo de
+            la conversación.
+          </p>
+        </details>
+        {data.question !== "" ? <InteractionView /> : <FirstQuestionView />}
+      </div>
+      {data.question !== "" && (
+        <div
+          className={
+            fullscreen
+              ? "z-50 absolute w-screen h-full bg-white -l-5 top-0 left-0"
+              : "w-full relative mx-auto max-w-3xl h-[600px] xl:h-auto rounded border-2"
+          }
+        >
+          <button
+            onClick={() => toggle()}
+            className={"absolute z-50 right-2 top-2"}
+          >
+            {fullscreen ? <X size={20} /> : <Maximize size={20} />}
+          </button>
+          <Graph />
+        </div>
+      )}
+    </div>
+  );
 }
